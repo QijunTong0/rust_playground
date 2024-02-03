eb90360ad6a41d2146cfe7b42c14ad439b94a1f3
QijunTong <40225103+QijunTong0@users.noreply.github.com>
2024-02-03 14:38:01 +0900

    iter

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
 fn main() {
-    let nums: Vec<i32> = vec![0, 1, 2, 3];
+    let nums: Vec<i32> = vec![1, 2, 3, 4];
     for x in nums.iter() {
-        println!("{}", x);
+        println!("{}", x * 2);
     }
+    nums.iter().for_each(|x: &i32| println!("{}", x));
+    let count: usize = nums.iter().map(|x: &i32| x + 10).count();
+    let sum: i32 = nums.iter().map(|x: &i32| x + 10).sum();
+    println!("{},{}", count, sum);
+    let prod = (1..10).fold(1, |reduction, x| reduction * x);
+    println!("{}", prod);
+    let nums2: Vec<&i32> = nums.iter().filter(|x: &&i32| *x % 2 == 0).collect();
 }
