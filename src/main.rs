d605289f7a878fc898dec970a567409d0a6ad140
QijunTong <40225103+QijunTong0@users.noreply.github.com>
2024-02-10 03:40:27 +0900

    mod

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
 fn main() {
-    print!("Hello World");
+    let vec_of_vec = vec![vec![1, 2, 3], vec![4, 5, 6], vec![7, 8, 9]];
+    let inner_vec = &vec_of_vec[0];
+    println!("{:?}", inner_vec);
 }
