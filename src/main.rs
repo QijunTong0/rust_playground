840749dc56825593987b13ed8639d556dcafdcb8
QijunTong <40225103+QijunTong0@users.noreply.github.com>
2024-03-21 01:10:42 +0900

    trait

--- src/main.rs
+++ src/main.rs
@@ -18,13 +18,33 @@ fn main() {
         .map(|x: i64| (x - 1) as usize)
         .collect();
     let n: usize = df[0].name().parse().unwrap();
-    let mut edges: Vec<Vec<usize>> = vec![vec![]; n];
+    let mut edges: Vec<Vec<usize>> = vec![vec![]; 22756];
     for (a, b) in edge_from.iter().zip(edge_to.iter()) {
         edges[*a].push(*b);
     }
     let topological_order: Vec<usize> = topological_sort(n, &edges);
-}
+    let mut reorder_map: Vec<usize> = vec![0; n];
+    for i in 0..n {
+        reorder_map[topological_order[i]] = i;
+    }
 
+    let mut tasks: Vec<Task> = Vec::with_capacity(0);
+    for i in 0..n {
+        edges[i].iter_mut().for_each(|x| *x = reorder_map[*x]);
+        tasks.push(Task {
+            duration: ((i + 1) % 1000usize) as i32,
+            st_time: 0,
+            ed_time: 0,
+            dependency: edges[i].clone(),
+        });
+        if i % 5000 == 0 {
+            println!("duration:={}", tasks[i].duration);
+            println!("st_time:={}", tasks[i].st_time);
+            println!("ed_time:={}", tasks[i].ed_time);
+            println!("dependency:={:?}", tasks[i].dependency);
+        }
+    }
+}
 struct Task {
     duration: i32,
     st_time: i32,
